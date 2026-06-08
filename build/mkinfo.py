204a0efd27ceb2fb706cde08ee562215943300d7
Bernhard Kaindl <bernhard.kaindl@citrix.com>
2026-06-08 12:00:00 +0000

    Add isort, black and flake8 for Python formating & CI
    Signed-off-by: Bernhard Kaindl <bernhard.kaindl@citrix.com>

--- build/mkinfo.py
+++ build/mkinfo.py
@@ -12,14 +12,12 @@ template = {
     "category": cat,
     "environments": [],
     "variations": [],
-    }
+}
 
 if envs:
     template["environments"] = envs.split(" ")
 if variations:
     template["variations"] = variations.split(" ")
 
-open(out, "w").write(
-    json.dumps(template, indent=4, separators=(',', ': '))
-    + "\n"
-    )
+with open(out, "w") as f:
+    f.write(json.dumps(template, indent=4, separators=(",", ": ")) + "\n")
