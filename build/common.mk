9d906ddf482d35a119168a233317490a419ea862
Bernhard Kaindl <bernhard.kaindl@citrix.com>
2026-06-11 12:00:00 +0000

    Create new XTF test category nested-svm for nested SVM tests
    With the new test category, all nested SVM tests can be run using:
    ./xtf-runner nested-svm
    
    Signed-off-by: Bernhard Kaindl <bernhard.kaindl@citrix.com>

--- build/common.mk
+++ build/common.mk
@@ -6,7 +6,7 @@ ifeq ($(XTF_METADATA_ONLY),1)
 
 else
 
-ALL_CATEGORIES     := special functional xsa utility in-development
+ALL_CATEGORIES     := special functional xsa utility in-development nested-svm
 
 ALL_ENVIRONMENTS   := pv64 pv32pae hvm64 hvm32pae hvm32pse hvm32
 
@@ -14,6 +14,7 @@ PV_ENVIRONMENTS    := $(filter pv%,$(ALL_ENVIRONMENTS))
 HVM_ENVIRONMENTS   := $(filter hvm%,$(ALL_ENVIRONMENTS))
 32BIT_ENVIRONMENTS := $(filter pv32% hvm32%,$(ALL_ENVIRONMENTS))
 64BIT_ENVIRONMENTS := $(filter pv64% hvm64%,$(ALL_ENVIRONMENTS))
+SVM_ENVIRONMENTS   := hvm64
 
 # $(env)_guest => pv or hvm mapping
 $(foreach env,$(PV_ENVIRONMENTS),$(eval $(env)_guest := pv))
